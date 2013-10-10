ac767a6600077a5a4777edc0fe659ca9b491595d
Taylor Gorman <me@taylorpatrickgorman.com>
2013-10-10 10:46:09 -0400

    Changed header again. I gotta stop redoing stuff..

--- designs/scripts/all.js
+++ designs/scripts/all.js
@@ -1,8 +1,13 @@
 
 jQuery(function($){
 
+	$('nav.view').on('click', 'a', function(){
+		$(this).siblings('a').removeClass('selected');
+		$(this).addClass('selected');
+	});
 	$('nav.messages-toggle').on('click', 'a', function(){
 		$('.messages').toggleClass('hide');
+		$(this).toggleClass('selected');
 	});
 
 });
